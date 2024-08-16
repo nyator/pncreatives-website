4ab89668ec41bbc8673dc1ce4c025e886d012788
Henry <hhen5562@gmail.com>
2024-08-16 16:41:20 +0000

    "Added react-router-dom dependency and implemented routing in main.jsx; removed Landing.jsx file"

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,26 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
+import App from "./App.jsx"
+import LandingPage from "../src/pages/LandingPage.jsx"
+import Portfolio from "../src/pages/Portfolio.jsx"
+import ComingSoon from "../src/pages/ComingSoon.jsx"
+import NotFound from "./pages/NotFound.jsx";
+
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+    errorElement: <NotFound />,
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
