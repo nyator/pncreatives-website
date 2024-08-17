f22e0eb285a125c19d1dfe3c6d08cef0da2553c1
Henry <hhen5562@gmail.com>
2024-08-17 04:49:23 +0000

    Done with Navigation Bar Yet To Do : Routing navigation tabs

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import { AnimatePresence, motion } from "framer-motion";
 
 import { logo } from "../constants/assets.js";
 import { CgMenuMotion } from "react-icons/cg";
@@ -17,6 +18,53 @@ const Navbar = () => {
     setOpen((prevOpen) => !prevOpen);
   };
 
+  const menuVars = {
+    initial: {
+      scaleY: 0,
+    },
+    animate: {
+      scaleY: 1,
+      transition: {
+        duration: 0.9,
+        ease: [0.22, 1, 0.36, 1],
+      },
+    },
+    exit: {
+      opacity: 0,
+      transition: {
+        duration: 0.3,
+      },
+    },
+  };
+
+  const mobileLinkVars = {
+    initial: {
+      y: "30vh",
+      transition: {
+        duration: 0.1,
+      },
+    },
+    open: {
+      y: 0,
+      transition: {
+        duration: 0.3,
+        ease: [0.22, 1, 9.36, 0],
+      },
+    },
+  };
+
+  const mobNav = {
+    initial: {
+      opacity: 0,
+    },
+    animate: {
+      opacity: 1,
+    },
+    exit: {
+      opacity: 0,
+    },
+  };
+
   return (
     <>
       <div className="flex bg-red-400 font-cVariable w-4/5 mx-auto justify-between items-center pl-8 pr-4 py-3 mt-10 rounded-full">
@@ -24,7 +72,7 @@ const Navbar = () => {
           <img src={logo} alt="pncreatives" className="size-8" />
         </div>
         <div className="hidden md:flex space-x-[60px] bg-gray py-2 px-5 font-cMedium text-navblack text-[13px] rounded-full">
-          {navLinks.map((items,index) => {
+          {navLinks.map((items, index) => {
             return (
               <>
                 <li key={index} className="list-none">
@@ -39,43 +87,63 @@ const Navbar = () => {
           <a href="">Contact US</a>
         </button>
 
-
-
-
         <button className="md:hidden" onClick={toggleMenu}>
           <CgMenuMotion className="size-7 text-primary" />
         </button>
 
-        {open && (
-          <div className="fixed md:hidden h-screen w-full left-0 top-0 bg-gray">
-            <div className="flex w-4/5 mx-auto justify-between items-center pl-8 pr-4 py-3 mt-10 rounded-full">
-              <div>
-                <img src={logo} alt="pncreatives" className="size-8" />
-              </div>
-              <button className="md:hidden" onClick={toggleMenu}>
-                <AiFillCloseCircle className="size-7 text-primary" />
-              </button>
-            </div>
+        <AnimatePresence>
+          {open && (
+            <motion.div
+              variants={mobNav}
+              initial="initial"
+              animate="animate"
+              exit="exit"
+              className="fixed md:hidden h-screen w-full origin-top left-0 top-0 bg-gray"
+            >
+              <div className="flex w-4/5 mx-auto justify-between items-center pl-8 pr-4 py-3 mt-10 rounded-full">
+                <div>
+                  <img src={logo} alt="pncreatives" className="size-8" />
+                </div>
 
-            <div className="h-full flex flex-col items-center justify-start px-10 gap-10 mt-20">
-              {navLinks.map((items, index) => {
-                return (
-                  <>
-                    <li
-                      key={index}
-                      className="list-none flex justify-center items-center w-fit text-[50px] hover:underline underline-offset-5 transition-all duration-500 ease-in-out"
-                    >
-                      <Link to={items.href}>{items.title}</Link>
-                    </li>
-                  </>
-                );
-              })}
-              <button className="bg-secondary-yray py-2 px-5 font-cMedium text-navblack text-[20px] rounded-full hover:scale-150 hover:bg-secondary-default hover:border-2 border-dashed border-gray transition-all duration-500 ease-in-out ">
-                <a href="">Contact US</a>
-              </button>
-            </div>
-          </div>
-        )}
+                <button className="md:hidden" onClick={toggleMenu}>
+                  <AiFillCloseCircle className="size-7 text-primary" />
+                </button>
+              </div>
+
+              <motion.div
+                variants={menuVars}
+                initial="initial"
+                animate="animate"
+                exit="exit"
+                className="h-full flex flex-col items-center justify-start px-10 gap-10 mt-20"
+              >
+                {navLinks.map((items, index) => {
+                  return (
+                    <>
+                      <div className="overflow-hidden">
+                        <motion.div
+                          variants={mobileLinkVars}
+                          initial="initial"
+                          animate="open"
+                          key={index}
+                          className="list-none flex justify-center items-center w-fit text-[50px] text-navblack hover:text-primary transition-all duration-300 ease-in-out"
+                        >
+                          <Link to={items.href}>{items.title}</Link>
+                        </motion.div>
+                      </div>
+                    </>
+                  );
+                })}
+                <button className="bg-secondary-yray py-2 px-5 font-cMedium text-navblack text-[20px] rounded-full hover:scale-150 hover:bg-secondary-default hover:border-2 border-dashed border-gray transition-all duration-500 ease-in-out ">
+                  <a href="">Contact US</a>
+                </button>
+                <p className="font-cExtrathin absolute bottom-10 z-10 text-[#a8a7a7] text-[12px] ">
+                  ©PN Creatives
+                </p>
+              </motion.div>
+            </motion.div>
+          )}
+        </AnimatePresence>
       </div>
     </>
   );
